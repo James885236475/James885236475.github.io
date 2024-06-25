1c12f4527d2709113bf5e2bfd6baea18450ae058
James Crouch <james.crouch@Jpschools.org>
2024-06-25 13:47:07 +0000

    saving data shapes

--- asd-projects/image-filtering/index.js
+++ asd-projects/image-filtering/index.js
@@ -20,7 +20,9 @@ function resetAndRender() {
 // all of your apply functions
 function applyAndRender() {
   // Multiple TODOs: Call your apply function(s) here
-
+  applyFilter(reddify);
+  applyFilterNoBackground(decreaseBlue);
+  applyFilterNoBackground(increaseGreenByBlue);
   
 
   // do not change the below line of code
@@ -33,17 +35,65 @@ function applyAndRender() {
 
 // TODO 1, 2 & 4: Create the applyFilter function here
 
+function applyFilter(filterFunction){
+
+  for(var i = 0; i < image.length; i++){
+    var row = image[i];
+    for (var j = 0; j < row.length; j++){
+      var rgbString = image[i][j];
+      var rgbNumbers = rgbStringToArray(rgbString);
+      filterFunction(rgbNumbers);
+      //rgbNumbers[RED] = 255;
+      rgbString = rgbArrayToString(rgbNumbers);
+      image[i][j] = rgbString;
+    }
+  }
+}
 
 // TODO 7: Create the applyFilterNoBackground function
+function applyFilterNoBackground(filterFunction){
+  var backgroundColor = image[0][0];
 
+  for(var i = 0; i < image.length; i++){
+    var row = image[i];
+    for (var j = 0; j < row.length; j++){
+      var rgbString = image[i][j];
+      
+      if (rgbString === backgroundColor){
+          
+      }else{
+        var rgbNumbers = rgbStringToArray(rgbString);
+        filterFunction(rgbNumbers);
+        rgbString = rgbArrayToString(rgbNumbers);
+        image[i][j] = rgbString;
+      }
+    }
+  }
+}
 
 // TODO 5: Create the keepInBounds function
 
 
+function keepInBounds(num){
+  return (num < 0 )? num = 0 : (num > 225)? num = 225: num = num;
+}
+
+
 // TODO 3: Create reddify function
 
+function reddify (arr){
+  arr[RED] = 200; 0
+}
 
-// TODO 6: Create more filter functions
 
+
+
+function decreaseBlue(array){
+  array[BLUE] = keepInBounds(array[BLUE] - 50); 
+}
+
+function increaseGreenByBlue(list){
+list[GREEN] = keepInBounds(list[GREEN] + list[BLUE]);
+}
 
 // CHALLENGE code goes below here
